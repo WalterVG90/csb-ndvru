e025bdf7443a87d075db15c05812cdbf0d7c8701
Code Sandbox <hello@codesandbox.io>
2022-01-30 21:04:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.48d13f9dfe259b8797f79cec3b8fe52e.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc4dff064d689b27c4d04173c49c0787",
+    "url": "https://waltervg90.github.io/csb-ndvru/index.html"
+  },
+  {
+    "revision": "022472cdff6c42d5847f",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/css/main.8e1e16fb.chunk.css"
+  },
+  {
+    "revision": "9769db7e65959e591a0f",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/js/2.92ff60f5.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/js/2.92ff60f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "022472cdff6c42d5847f",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/js/main.052ea355.chunk.js"
+  },
+  {
+    "revision": "433c2b0c36f36ab44137",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/js/runtime-main.fb9f57dd.js"
+  },
+  {
+    "revision": "2c685e9d2e4cd51841b855216a68a154",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/ChatKharaMeal-Website.2c685e9d.png"
+  },
+  {
+    "revision": "8b3eed34ccd88f5dbed1537163fe5ab1",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/Empolyee-table.8b3eed34.png"
+  },
+  {
+    "revision": "e0d4f3837450e809ff376dac878318d6",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/Expense-TrackerApp.e0d4f383.png"
+  },
+  {
+    "revision": "a97c0df096018748c5f2fa4b3af0a7ea",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/Fb-component.a97c0df0.png"
+  },
+  {
+    "revision": "efc84b727ddd6a549bb13375cd45d615",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/Music-List-App.efc84b72.png"
+  },
+  {
+    "revision": "6429306807ffbd6e3b9a95f3a0a59786",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/QuizzApp-JavaScript.64293068.png"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "cc96ba791c359e3d18524aa77eb1cbb9",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/blood-donate-app.cc96ba79.jpeg"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "10b0719343408095f5a9b369e0787115",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/firebase-icon.10b07193.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "567f006b9ca974f343db1c9c79920074",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/github.567f006b.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "38f9a4f29ee819486c821b193f592aed",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/logo-vercel.38f9a4f2.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "ecb5ec57f305689a6dc44a0828e4b893",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/netlify-icon.ecb5ec57.svg"
+  },
+  {
+    "revision": "d8fee7924d463334d60c5bf965237552",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/profile.d8fee792.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "760a3ee424f40cd7b8da50ac4818592f",
+    "url": "https://waltervg90.github.io/csb-ndvru/static/media/tailwindcss-icon.760a3ee4.svg"
+  }
+]);
